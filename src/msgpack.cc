d40c4a3bb493f649450c583c746ef7d9a4a77275
CKzcb <zhuchb_0509@163.com>
2023-06-09 18:04:50 +0800

    fix: make the module context-aware to fix can not self-register for nodejs

--- src/msgpack.cc
+++ src/msgpack.cc
@@ -17,61 +17,68 @@ using namespace node;
 
 // MSC does not support C99 trunc function.
 #ifdef _MSC_BUILD
-double trunc(double d){ return (d>0) ? floor(d) : ceil(d) ; }
+double trunc(double d)
+{
+    return (d > 0) ? floor(d) : ceil(d);
+}
 #endif
-
 
 static Nan::Persistent<FunctionTemplate> msgpack_unpack_template;
 
-
 // An exception class that wraps a textual message
-class MsgpackException {
-    public:
-        MsgpackException(const char *str) :
-            msg(Nan::New<String>(str).ToLocalChecked()) {
-        }
+class MsgpackException
+{
+public:
+    MsgpackException(const char *str) : msg(Nan::New<String>(str).ToLocalChecked())
+    {
+    }
 
-        Local<Value> getThrownException() {
-            return Nan::TypeError(msg);
-        }
+    Local<Value> getThrownException()
+    {
+        return Nan::TypeError(msg);
+    }
 
-    private:
-        const Local<String> msg;
+private:
+    const Local<String> msg;
 };
 
 // A holder for a msgpack_zone object; ensures destruction on scope exit
-class MsgpackZone {
-    public:
-        msgpack_zone _mz;
+class MsgpackZone
+{
+public:
+    msgpack_zone _mz;
 
-        MsgpackZone(size_t sz = 1024) {
-            msgpack_zone_init(&this->_mz, sz);
-        }
+    MsgpackZone(size_t sz = 1024)
+    {
+        msgpack_zone_init(&this->_mz, sz);
+    }
 
-        ~MsgpackZone() {
-            msgpack_zone_destroy(&this->_mz);
-        }
+    ~MsgpackZone()
+    {
+        msgpack_zone_destroy(&this->_mz);
+    }
 };
 
 static stack<msgpack_sbuffer *> sbuffers;
 
-#define DBG_PRINT_BUF(buf, name) \
-    do { \
-        fprintf(stderr, "Buffer %s has %lu bytes:\n", \
-            (name), Buffer::Length(buf) \
-        ); \
-        for (uint32_t i = 0; i * 16 < Buffer::Length(buf); i++) { \
-            fprintf(stderr, "  "); \
-            for (uint32_t ii = 0; \
+#define DBG_PRINT_BUF(buf, name)                                 \
+    do                                                           \
+    {                                                            \
+        fprintf(stderr, "Buffer %s has %lu bytes:\n",            \
+                (name), Buffer::Length(buf));                    \
+        for (uint32_t i = 0; i * 16 < Buffer::Length(buf); i++)  \
+        {                                                        \
+            fprintf(stderr, "  ");                               \
+            for (uint32_t ii = 0;                                \
                  ii < 16 && (i * 16) + ii < Buffer::Length(buf); \
-                 ii++) { \
-                fprintf(stderr, "%s%2.2hhx", \
-                    (ii > 0 && (ii % 2 == 0)) ? " " : "", \
-                    Buffer::Data(buf)[i * 16 + ii] \
-                ); \
-            } \
-            fprintf(stderr, "\n"); \
-        } \
+                 ii++)                                           \
+            {                                                    \
+                fprintf(stderr, "%s%2.2hhx",                     \
+                        (ii > 0 && (ii % 2 == 0)) ? " " : "",    \
+                        Buffer::Data(buf)[i * 16 + ii]);         \
+            }                                                    \
+            fprintf(stderr, "\n");                               \
+        }                                                        \
     } while (0)
 
 // This will be passed to Buffer::New so that we can manage our own memory.
@@ -79,13 +86,18 @@ static stack<msgpack_sbuffer *> sbuffers;
 // coding pattern before.  For now I have overloaded it to be a void pointer
 // to a msgpack_sbuffer.  This let's us push it onto the stack for use later.
 static void
-_free_sbuf(char *data, void *hint) {
-    if (data != NULL && hint != NULL) {
+_free_sbuf(char *data, void *hint)
+{
+    if (data != NULL && hint != NULL)
+    {
         msgpack_sbuffer *sbuffer = (msgpack_sbuffer *)hint;
         if (sbuffers.size() > SBUF_POOL ||
-            sbuffer->alloc > (MSGPACK_SBUFFER_INIT_SIZE * 5)) {
+            sbuffer->alloc > (MSGPACK_SBUFFER_INIT_SIZE * 5))
+        {
             msgpack_sbuffer_free(sbuffer);
-        } else {
+        }
+        else
+        {
             sbuffer->size = 0;
             sbuffers.push(sbuffer);
         }
@@ -99,70 +111,93 @@ _free_sbuf(char *data, void *hint) {
 //
 // If a circular reference is detected, an exception is thrown.
 static void
-v8_to_msgpack(Local<Value> v8obj, msgpack_object *mo, msgpack_zone *mz, size_t depth) {
-    if (512 < ++depth) {
+v8_to_msgpack(Local<Value> v8obj, msgpack_object *mo, msgpack_zone *mz, size_t depth)
+{
+    if (512 < ++depth)
+    {
         throw MsgpackException("Cowardly refusing to pack object with circular reference");
     }
 
-    if (v8obj->IsUndefined() || v8obj->IsNull()) {
+    if (v8obj->IsUndefined() || v8obj->IsNull())
+    {
         mo->type = MSGPACK_OBJECT_NIL;
-    } else if (v8obj->IsBoolean()) {
+    }
+    else if (v8obj->IsBoolean())
+    {
         mo->type = MSGPACK_OBJECT_BOOLEAN;
         mo->via.boolean = Nan::To<bool>(v8obj).FromJust();
-    } else if (v8obj->IsNumber()) {
+    }
+    else if (v8obj->IsNumber())
+    {
         double d = Nan::To<double>(v8obj).FromJust();
-        if (trunc(d) != d) {
+        if (trunc(d) != d)
+        {
             mo->type = MSGPACK_OBJECT_FLOAT;
             mo->via.f64 = d;
-        } else if (d > 0) {
+        }
+        else if (d > 0)
+        {
             mo->type = MSGPACK_OBJECT_POSITIVE_INTEGER;
             mo->via.u64 = static_cast<uint64_t>(d);
-        } else {
+        }
+        else
+        {
             mo->type = MSGPACK_OBJECT_NEGATIVE_INTEGER;
             mo->via.i64 = static_cast<int64_t>(d);
         }
-    } else if (v8obj->IsString()) {
+    }
+    else if (v8obj->IsString())
+    {
         mo->type = MSGPACK_OBJECT_STR;
         mo->via.str.size = static_cast<uint32_t>(Nan::DecodeBytes(v8obj, Nan::Encoding::UTF8));
-        mo->via.str.ptr = (char*) msgpack_zone_malloc(mz, mo->via.str.size);
+        mo->via.str.ptr = (char *)msgpack_zone_malloc(mz, mo->via.str.size);
 
-        Nan::DecodeWrite((char*)mo->via.str.ptr, mo->via.str.size, v8obj, Nan::Encoding::UTF8);
-    } else if (v8obj->IsDate()) {
+        Nan::DecodeWrite((char *)mo->via.str.ptr, mo->via.str.size, v8obj, Nan::Encoding::UTF8);
+    }
+    else if (v8obj->IsDate())
+    {
         mo->type = MSGPACK_OBJECT_STR;
         Local<Date> date = Local<Date>::Cast(v8obj);
         Local<Function> func = Local<Function>::Cast(Nan::Get(date, Nan::New<String>("toISOString").ToLocalChecked()).ToLocalChecked());
         Local<Value> argv[1] = {};
         Local<Value> result = Nan::Call(func, date, 0, argv).ToLocalChecked();
         mo->via.str.size = static_cast<uint32_t>(Nan::DecodeBytes(result, Nan::Encoding::UTF8));
-        mo->via.str.ptr = (char*) msgpack_zone_malloc(mz, mo->via.str.size);
+        mo->via.str.ptr = (char *)msgpack_zone_malloc(mz, mo->via.str.size);
 
-        Nan::DecodeWrite((char*)mo->via.str.ptr, mo->via.str.size, result, Nan::Encoding::UTF8);
-    } else if (v8obj->IsArray()) {
+        Nan::DecodeWrite((char *)mo->via.str.ptr, mo->via.str.size, result, Nan::Encoding::UTF8);
+    }
+    else if (v8obj->IsArray())
+    {
         Local<Object> o = Nan::To<v8::Object>(v8obj).ToLocalChecked();
         Local<Array> a = Local<Array>::Cast(o);
 
         mo->type = MSGPACK_OBJECT_ARRAY;
         mo->via.array.size = a->Length();
-        mo->via.array.ptr = (msgpack_object*) msgpack_zone_malloc(
+        mo->via.array.ptr = (msgpack_object *)msgpack_zone_malloc(
             mz,
-            sizeof(msgpack_object) * mo->via.array.size
-        );
+            sizeof(msgpack_object) * mo->via.array.size);
 
-        for (uint32_t i = 0; i < a->Length(); i++) {
+        for (uint32_t i = 0; i < a->Length(); i++)
+        {
             Local<Value> v = Nan::Get(a, i).ToLocalChecked();
             v8_to_msgpack(v, &mo->via.array.ptr[i], mz, depth);
         }
-    } else if (Buffer::HasInstance(v8obj)) {
+    }
+    else if (Buffer::HasInstance(v8obj))
+    {
         Local<Object> buf = Nan::To<Object>(v8obj).ToLocalChecked();
 
         mo->type = MSGPACK_OBJECT_BIN;
         mo->via.bin.size = static_cast<uint32_t>(Buffer::Length(buf));
         mo->via.bin.ptr = Buffer::Data(buf);
-    } else {
+    }
+    else
+    {
         Local<Object> o = Nan::To<Object>(v8obj).ToLocalChecked();
         Local<String> toJSON = Nan::New<String>("toJSON").ToLocalChecked();
         // for o.toJSON()
-        if (Nan::Has(o, toJSON).FromJust() && Nan::Get(o, toJSON).ToLocalChecked()->IsFunction()) {
+        if (Nan::Has(o, toJSON).FromJust() && Nan::Get(o, toJSON).ToLocalChecked()->IsFunction())
+        {
             Local<Function> fn = Local<Function>::Cast(Nan::Get(o, toJSON).ToLocalChecked());
             v8_to_msgpack(Nan::Call(fn, o, 0, NULL).ToLocalChecked(), mo, mz, depth);
             return;
@@ -172,12 +207,12 @@ v8_to_msgpack(Local<Value> v8obj, msgpack_object *mo, msgpack_zone *mz, size_t d
 
         mo->type = MSGPACK_OBJECT_MAP;
         mo->via.map.size = a->Length();
-        mo->via.map.ptr = (msgpack_object_kv*) msgpack_zone_malloc(
+        mo->via.map.ptr = (msgpack_object_kv *)msgpack_zone_malloc(
             mz,
-            sizeof(msgpack_object_kv) * mo->via.map.size
-        );
+            sizeof(msgpack_object_kv) * mo->via.map.size);
 
-        for (uint32_t i = 0; i < a->Length(); i++) {
+        for (uint32_t i = 0; i < a->Length(); i++)
+        {
             Local<Value> k = Nan::Get(a, i).ToLocalChecked();
 
             v8_to_msgpack(k, &mo->via.map.ptr[i].key, mz, depth);
@@ -191,15 +226,15 @@ v8_to_msgpack(Local<Value> v8obj, msgpack_object *mo, msgpack_zone *mz, size_t d
 // This method is recursive. It will probably blow out the stack on objects
 // with extremely deep nesting.
 static Local<Value>
-msgpack_to_v8(msgpack_object *mo) {
-    switch (mo->type) {
+msgpack_to_v8(msgpack_object *mo)
+{
+    switch (mo->type)
+    {
     case MSGPACK_OBJECT_NIL:
         return Nan::Null();
 
     case MSGPACK_OBJECT_BOOLEAN:
-        return (mo->via.boolean) ?
-            Nan::True() :
-            Nan::False();
+        return (mo->via.boolean) ? Nan::True() : Nan::False();
 
     case MSGPACK_OBJECT_POSITIVE_INTEGER:
         // As per Issue #42, we need to use the base Number
@@ -215,10 +250,12 @@ msgpack_to_v8(msgpack_object *mo) {
     case MSGPACK_OBJECT_FLOAT:
         return Nan::New<Number>(mo->via.f64);
 
-    case MSGPACK_OBJECT_ARRAY: {
+    case MSGPACK_OBJECT_ARRAY:
+    {
         Local<Array> a = Nan::New<Array>(mo->via.array.size);
 
-        for (uint32_t i = 0; i < mo->via.array.size; i++) {
+        for (uint32_t i = 0; i < mo->via.array.size; i++)
+        {
             Nan::Set(a, i, msgpack_to_v8(&mo->via.array.ptr[i]));
         }
 
@@ -230,15 +267,16 @@ msgpack_to_v8(msgpack_object *mo) {
 
     case MSGPACK_OBJECT_BIN:
         return Nan::CopyBuffer(mo->via.str.ptr, mo->via.bin.size).ToLocalChecked();
-    case MSGPACK_OBJECT_MAP: {
+    case MSGPACK_OBJECT_MAP:
+    {
         Local<Object> o = Nan::New<Object>();
 
-        for (uint32_t i = 0; i < mo->via.map.size; i++) {
+        for (uint32_t i = 0; i < mo->via.map.size; i++)
+        {
             Nan::Set(
                 o,
                 msgpack_to_v8(&mo->via.map.ptr[i].key),
-                msgpack_to_v8(&mo->via.map.ptr[i].val)
-            );
+                msgpack_to_v8(&mo->via.map.ptr[i].val));
         }
 
         return o;
@@ -257,39 +295,48 @@ msgpack_to_v8(msgpack_object *mo) {
 //
 // Any number of objects can be provided as arguments, and all will be
 // serialized to the same bytestream, back-to-back.
-static NAN_METHOD(pack) {
+static NAN_METHOD(pack)
+{
     Nan::HandleScope scope;
 
     msgpack_packer pk;
     MsgpackZone mz;
     msgpack_sbuffer *sb;
 
-    if (!sbuffers.empty()) {
+    if (!sbuffers.empty())
+    {
         sb = sbuffers.top();
         sbuffers.pop();
-    } else {
+    }
+    else
+    {
         sb = msgpack_sbuffer_new();
     }
 
     msgpack_packer_init(&pk, sb, msgpack_sbuffer_write);
 
-    for (int i = 0; i < info.Length(); i++) {
+    for (int i = 0; i < info.Length(); i++)
+    {
         msgpack_object mo;
 
-        try {
+        try
+        {
             v8_to_msgpack(info[i], &mo, &mz._mz, 0);
-        } catch (MsgpackException e) {
+        }
+        catch (MsgpackException e)
+        {
             return Nan::ThrowError(e.getThrownException());
         }
 
-        if (msgpack_pack_object(&pk, mo)) {
+        if (msgpack_pack_object(&pk, mo))
+        {
             return Nan::ThrowError("Error serializaing object");
         }
     }
 
     Local<Object> slowBuffer = Nan::NewBuffer(
-        sb->data, sb->size, _free_sbuf, (void *)sb
-    ).ToLocalChecked();
+                                   sb->data, sb->size, _free_sbuf, (void *)sb)
+                                   .ToLocalChecked();
 
     return info.GetReturnValue().Set(slowBuffer);
 }
@@ -299,10 +346,12 @@ static NAN_METHOD(pack) {
 // Return the JavaScript object resulting from unpacking the contents of the
 // specified buffer. If the buffer does not contain a complete object, the
 // undefined value is returned.
-static NAN_METHOD(unpack) {
+static NAN_METHOD(unpack)
+{
     Nan::HandleScope scope;
 
-    if (info.Length() < 0 || !Buffer::HasInstance(info[0])) {
+    if (info.Length() < 0 || !Buffer::HasInstance(info[0]))
+    {
         return Nan::ThrowTypeError("First argument must be a Buffer");
     }
 
@@ -312,17 +361,20 @@ static NAN_METHOD(unpack) {
     msgpack_object mo;
     size_t off = 0;
 
-    switch (msgpack_unpack(Buffer::Data(buf), Buffer::Length(buf), &off, &mz._mz, &mo)) {
+    switch (msgpack_unpack(Buffer::Data(buf), Buffer::Length(buf), &off, &mz._mz, &mo))
+    {
     case MSGPACK_UNPACK_EXTRA_BYTES:
     case MSGPACK_UNPACK_SUCCESS:
-        try {
+        try
+        {
             Nan::Set(
                 Nan::GetFunction(Nan::New<FunctionTemplate>(msgpack_unpack_template)).ToLocalChecked(),
                 Nan::New<String>("bytes_remaining").ToLocalChecked(),
-                Nan::New<Integer>(static_cast<int32_t>(Buffer::Length(buf) - off))
-            );
+                Nan::New<Integer>(static_cast<int32_t>(Buffer::Length(buf) - off)));
             return info.GetReturnValue().Set(msgpack_to_v8(&mo));
-        } catch (MsgpackException e) {
+        }
+        catch (MsgpackException e)
+        {
             return Nan::ThrowError(e.getThrownException());
         }
 
@@ -334,7 +386,8 @@ static NAN_METHOD(unpack) {
     }
 }
 
-NAN_MODULE_INIT(init) {
+NAN_MODULE_INIT(init)
+{
     Nan::Set(target, Nan::New<String>("pack").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(pack)).ToLocalChecked());
 
     // Go through this mess rather than call NODE_SET_METHOD so that we can set
@@ -344,9 +397,14 @@ NAN_MODULE_INIT(init) {
     Nan::Set(
         target,
         Nan::New<String>("unpack").ToLocalChecked(),
-        Nan::GetFunction(Nan::New<FunctionTemplate>(msgpack_unpack_template)).ToLocalChecked()
-    );
+        Nan::GetFunction(Nan::New<FunctionTemplate>(msgpack_unpack_template)).ToLocalChecked());
 }
 
-NODE_MODULE(msgpackBinding, init);
+// NODE_MODULE(msgpackBinding, init);
+// fix: make the module context-aware that it needs to reolace NODE_MODULE
+// issue: https://github.com/nodejs/node/issues/21783#issuecomment-429637117
+NODE_MODULE_INIT()
+{
+    init(exports);
+}
 // vim:ts=4 sw=4 et
